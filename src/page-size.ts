5f3728dd7849a007913acac79c6f38fd4feb1264
Marc Gavanier <marc@gavanier.com>
2026-04-19 14:17:40 +0200

    style: fix import order for biome

--- src/page-size.ts
+++ src/page-size.ts
@@ -1,5 +1,5 @@
-import { brand, greaterThanOrEqualTo, int, Number as SchemaNumber } from 'effect/Schema';
 import { defineModel, type Model } from '@arckit/effect';
+import { brand, greaterThanOrEqualTo, int, Number as SchemaNumber } from 'effect/Schema';
 
 export const PageSize = defineModel(SchemaNumber.pipe(int(), greaterThanOrEqualTo(1), brand('PageSize')), (value) =>
   Math.max(1, Math.round(value))
